3b2cbbb99f75472875c96c45e8ec390a3300e3ac
namchee <cristophernamchee12@gmail.com>
2018-10-17 16:28:29 +0700

    Fixing rookie mistakes

--- src/app/view/AboutController.java
+++ src/app/view/AboutController.java
@@ -3,8 +3,6 @@ package app.view;
 import app.App;
 import javafx.fxml.FXML;
 import javafx.scene.control.Hyperlink;
-import javafx.scene.web.WebEngine;
-import javafx.scene.web.WebView;
 
 public class AboutController {
     @FXML
@@ -19,9 +17,6 @@ public class AboutController {
     
     @FXML
     private void handleLink() {
-        System.out.println("test");
-        WebView view = new WebView();
-        final WebEngine engine = view.getEngine();
         this.app.getHostServices().showDocument("https://github.com/Namchee/wombatik");
     }
     
